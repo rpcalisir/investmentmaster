565cb7a33154eef20275bb0d6479905fced0b74f
Recep Çalışır <rpcalisir@gmail.com>
2022-09-28 23:00:28 +0300

    Layers are created for main scaffold.

---
+++ InvestmentMaster.BL.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace InvestmentMaster.BL.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
