230e17ae237e077eafb81517899fc59446b4ad0f
Recep Çalışır <rpcalisir@gmail.com>
2022-10-05 16:53:27 +0300

    Changed from EntityFramework to EntityFrameworkCore

--- InvestmentMaster.Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
+++ InvestmentMaster.Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
@@ -1,7 +1,7 @@
 ﻿using InvestmentMaster.Core.Entities;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
-using System.Data.Entity;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
--- InvestmentMaster.Core/DataAccess/EntityFramework/EfQueryableRepository.cs
+++ InvestmentMaster.Core/DataAccess/EntityFramework/EfQueryableRepository.cs
@@ -1,7 +1,7 @@
 ﻿using InvestmentMaster.Core.Entities;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
-using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +11,7 @@ namespace InvestmentMaster.Core.DataAccess.EntityFramework
     public class EfQueryableRepository<T> : IQueryableRepository<T> where T : class, IEntity, new()
     {
         private DbContext _context;
-        private IDbSet<T> _entities;
+        private DbSet<T>? _entities;
 
         public EfQueryableRepository(DbContext context)
         {
@@ -20,14 +20,13 @@ namespace InvestmentMaster.Core.DataAccess.EntityFramework
 
         public IQueryable<T> Table => this._entities;
 
-        protected virtual IDbSet<T> Entities 
+        protected virtual DbSet<T> Entities 
         {
             get
             {
                 return _entities ??= _context.Set<T>();
             }
         }
-
 
     }
 }
